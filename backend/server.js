cda0a578de620cdce6dca0f436e06595c603ada2
BayandaJack <2669074@students.wits.ac.za>
2025-12-20 15:54:58 +0200

    fix(backend): Add env file and gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Server is starting...");
+//Require express and initialize an express app instance
+const express = require("express");
+const app = express();
+//Require dotenv to enable reading env vars from .env file
+require('dotenv').config();
+
+//setup listening port
+app.listen(() => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
