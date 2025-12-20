427034c0e11850629a152a50060bdb2b490a2454
BayandaJack <2669074@students.wits.ac.za>
2025-12-20 19:48:36 +0200

    feat(controllers): Add controller folder for MVC structure

---
+++ backend/controllers/tasks.js
@@ -0,0 +1,9 @@
+// require express for instance of express app
+const express = require('express');
+// require instance of express router
+const router = express.Router();
+
+// deal with different CRUD operations here
+router.get('/', (req, res) => {
+    
+})
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const app = express();
 //Require dotenv to enable reading env vars from .env file
 require('dotenv').config();
 
+//setup middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send("Hi");
+});
+
 //setup listening port
-app.listen(() => {
+app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
-})
+});
